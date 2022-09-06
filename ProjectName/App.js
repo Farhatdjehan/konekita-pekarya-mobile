55ae436fb25ae887c417f2b1a9ca573ebd97e97b
farhatdk <farhatdk98@gmail.com>
2022-09-06 11:33:08 +0700

    init konekita mobile

---
+++ ProjectName/App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import WebView from 'react-native-webview';
+
+
+const App: () => Node = () => {
+
+  return (
+    <>
+    <WebView source={{ uri: 'https://konekita.com/' }} />
+    </>
+  );
+};
+
+
+export default App;
